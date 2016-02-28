e689ea5bb0564437e1bedb0831b1d07b9f0467de
Duncan Beard <duncanmbeard@gmail.com>
2016-02-28 02:30:13 +0000

    Removed useless deploy script. Fixed the single test.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/').expect('Hello World!\n', done);
+        request(app).get('/').expect('Hello World, this is an update!\n', done);
     });
 });
