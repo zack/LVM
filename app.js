e886b7d5eb70e82f9ccc94fe9cf1383896180de9
Duncan Beard <duncanmbeard@gmail.com>
2016-02-28 00:39:56 +0000

    Made an update to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World!\n'); });
+    res.send('Hello World, this is an update!\n'); });
 
 app.listen(8081);
 
