0abc2d9fd562dfa8fcf8f830b3f493aa0cc944f1
Mike Rodrigues <nismo9132@gmail.com>
2016-03-31 20:29:07 +0000

    Removing debug logs

--- app/lib/server.js
+++ app/lib/server.js
@@ -6,9 +6,7 @@
 'use strict';
 
 module.exports = function(app, config) {
-    console.log('ENV PORT', process.env.NODE_PORT);
     var port = process.env.NODE_PORT || config.port;
-    console.log('Actual', port);
     console.log('[express train application listening on %s]', port);
     return app.listen(port);
 };
