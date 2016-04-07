d5ab0d5079a055179e2b7a53aed36a6243042eae
Duncan Beard <duncan@duncanbeard.com>
2016-04-07 12:55:21 -0400

    Fixed some Dashboard routing stuff. Now dashboard.html is dashboard.hbs in our /views folder, pending our decision on whether to use Handlebars or not.

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -6,30 +6,33 @@
 'use strict';
 
 var pjson = require('../../package.json'),
-    _ = require('underscore');
+  _ = require('underscore');
 
-module.exports = function (envConfig, constants, statusCodes) {
-    var config = {
-        homeConfig: {
+module.exports = function(envConfig, constants, statusCodes) {
+  var config = {
+    homeConfig: {
 
-            version: pjson.version,
+      version: pjson.version,
 
-            envConfig: JSON.stringify(_.extend(envConfig, {
-                statusCodes: statusCodes
-            })),
+      envConfig: JSON.stringify(_.extend(envConfig, {
+        statusCodes: statusCodes
+      })),
 
-            constants: constants
-        }
-    };
-    
-    return {
-        // Landing & Login page
-        login: function (req, res, next) {
-            res.render('login', _.extend(JSON.parse(JSON.stringify(config)), req.query));
-        },
-        
-        dashboard: function (req, res, next) {
-            res.render('dashboard', _.extend(JSON.parse(JSON.stringify(config)), {user: req.session.user}));
-        }
-    };
+      constants: constants
+    }
+  };
+
+  return {
+    // Landing & Login page
+    login: function(req, res, next) {
+      res.render('login', _.extend(JSON.parse(JSON.stringify(config)), req.query));
+    },
+
+    dashboard: function(req, res, next) {
+      res.render('dashboard', _.extend(JSON.parse(JSON.stringify(
+        config)), {
+        user: req.session.user
+      }));
+    }
+  };
 };
