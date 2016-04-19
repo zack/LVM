31dea5d9957a9ab18d6e8cbd60c46f6827e275fc
anakorolev <korolev.a@husky.neu.edu>
2016-04-19 00:52:37 -0400

    remove old database files, fix rest db test data script to make sure it runs the correct scripts

--- scripts/continuousInt/dev-reset-db-test-data.sh
+++ scripts/continuousInt/dev-reset-db-test-data.sh
@@ -9,6 +9,7 @@ mysql --host=cs4500.duncanbeard.com --user=test --password=testing < 01LVMCreate
 
 # Create the tables
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 02LVMCreateTableScript.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 02LVMCreateTableZipCodes.sql
 
 # Alter tables
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 03LVMAlterTableDOEOccupationScript.sql
@@ -19,21 +20,33 @@ mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < LVMVali
 
 # Add test data
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateExitTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateMatchSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateMatchTableSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateMeetingTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdatePersonAvailabilityTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdatePersonSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdatePersonTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdatePersonPreferencesTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentAnswersTableSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentAssessmentTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentComputerSkillsTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentContactInfoTableSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentDependentsTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentDisabiltyAccommodationsTableSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentGoalsTableSproc.sql
-mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentPreferencesTableSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentPublicAssistanceTableSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateStudentWorkExperienceTableSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateTutorHoursTableSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 04LVMUpdateTutorTableSproc.sql
-mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 05LVMAddStudentSproc.sql
-mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 05LVMAddTutorSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 05LVMDeleteTestDataSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 06LVMAddExitRemoveMatchSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 06LVMAddMatchSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 06LVMAddMeetingSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 06LVMAddStudentSproc.sql
+mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 06LVMAddTutorSproc.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < 07LVMAddTestStudentTutor.sql
 
 # Validate test data was inserted
-mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < LVMValidateAlterTableInsert.sql
 mysql --host=cs4500.duncanbeard.com --user=test --password=testing lvm < LVMValidateTestDataInsert.sql
 
